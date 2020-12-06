0484fb7faae051f28d639668ee09c19b2d749544
Andrey Tatarinov <a@tatarinov.co>
2020-12-06 15:54:05 +0000

    prettify

--- 06/06.py
+++ 06/06.py
@@ -1,28 +1,22 @@
 #%%
-with open('input.txt', 'r') as f:
-    data = f.read()
+import pandas as pd
 
 #%%
-import pandas as pd
+with open('input.txt', 'r') as f:
+    data = f.read()
 
 # %%
 df = pd.DataFrame({'raw': data.split('\n\n')})
 df['answers'] = df['raw'].apply(lambda x: [i for i in x.split('\n') if i != ''])
+
+# %%
+### part 1
 df['n_unique'] = df['raw'].apply(lambda x: len(set(x.replace('\n', ''))))
-# %%
-df
-# %%
 df['n_unique'].sum() #6530
+
 # %%
-def intersect(l):
-    res = set(l[0])
-    for i in l[1:]:
-        res = res.intersection(set(i))
-    return len(res)
-# %%
-df['n_intersect'] = df['answers'].apply(intersect)
-# %%
-df
-# %%
+### part 2
+df['n_intersect'] = df['answers'].apply(lambda x: len(set.intersection(*[set(i) for i in x])))
 df['n_intersect'].sum() #3323
+
 # %%
