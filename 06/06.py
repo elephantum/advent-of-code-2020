f50a7dc5694e9bd632b42e3f317ae499d440a94c
Andrey Tatarinov <a@tatarinov.co>
2020-12-06 16:16:55 +0000

    simplify

--- 06/06.py
+++ 06/06.py
@@ -7,16 +7,16 @@ with open('input.txt', 'r') as f:
 
 # %%
 df = pd.DataFrame({'raw': data.split('\n\n')})
-df['answers'] = df['raw'].apply(lambda x: [i for i in x.split('\n') if i != ''])
+df['answers'] = df['raw'].apply(lambda x: [set(i) for i in x.split('\n') if i != ''])
 
 # %%
 ### part 1
-df['n_unique'] = df['raw'].apply(lambda x: len(set(x.replace('\n', ''))))
+df['n_unique'] = df['answers'].apply(lambda x: len(set.union(*x)))
 df['n_unique'].sum() #6530
 
 # %%
 ### part 2
-df['n_intersect'] = df['answers'].apply(lambda x: len(set.intersection(*[set(i) for i in x])))
+df['n_intersect'] = df['answers'].apply(lambda x: len(set.intersection(*x)))
 df['n_intersect'].sum() #3323
 
 # %%
