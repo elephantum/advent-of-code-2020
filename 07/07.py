90343f712ae972169a744900b7480f5d30cbf91c
Andrey Tatarinov <a@tatarinov.co>
2020-12-07 22:15:35 +0300

    07

--- 07/07.py
+++ 07/07.py
@@ -20,23 +20,18 @@ with open('input.txt', 'r') as f:
     df = pd.DataFrame({'raw': f.readlines()})
 # %%
 df['parsed'] = df['raw'].apply(rule.parseString)
-df['edges_wo_no'] = df['parsed'].apply(lambda x: [(i.what, x.what) for i in x.rule])
 df['edges_wgt'] = df['parsed'].apply(lambda x: [((x.what, i.what), i.n) for i in x.rule])
 # %%
 df
 # %%
 # part1
 G = nx.DiGraph()
-df['edges_wo_no'].apply(lambda x: [G.add_edge(e[0], e[1]) for e in x])
+df['edges_wgt'].apply(lambda x: [G.add_edge(e[0], e[1], n=n) for (e, n) in x])
 # %%
-len(nx.single_source_shortest_path(G, 'shiny gold')) - 1  # 222
-
+len(nx.single_source_shortest_path(G.reverse(), 'shiny gold')) - 1  # 222
 
 # %%
 # part2
-G = nx.DiGraph()
-df['edges_wgt'].apply(lambda x: [G.add_edge(e[0], e[1], n=n) for e,n in x])
-#%%
 def count_p2(G, n):
     return 1 + sum([count_p2(G, k)*v['n'] for (k, v) in G[n].items()])
 # %%
