a15e4344703ecd60478ec22e294ce3b4704c0cdc
Andrey Tatarinov <a@tatarinov.co>
2020-12-08 23:25:03 +0300

    08 graph 5ms

--- 08/08_2_graph.py
+++ 08/08_2_graph.py
@@ -2,8 +2,10 @@
 import pandas as pd
 # %%
 df = pd.read_csv('./input.txt', sep=' ', header=None, names=['op', 'arg'])
+ops = list(df['op'])
+args = list(df['arg'])
 #%%
-def run2(df):
+def run2(ops, args):
     states = [(0, 0, [], False)]
     while True:
         new_states = []
@@ -13,20 +15,20 @@ def run2(df):
             elif ip > len(df) or ip in visits:
                 continue
 
-            op = df.loc[ip, 'op']
+            op = ops[ip]
             if op == 'acc':
-                new_states.append((ip+1, acc+df.loc[ip, 'arg'], visits + [ip], mod))
+                new_states.append((ip+1, acc+args[ip], visits + [ip], mod))
             if op == 'jmp':
-                new_states.append((ip+df.loc[ip, 'arg'], acc, visits + [ip], mod))
+                new_states.append((ip+args[ip], acc, visits + [ip], mod))
                 if not mod:
                     new_states.append((ip+1, acc, visits + [ip], True))
             if op == 'nop':
                 new_states.append((ip+1, acc, visits + [ip], mod))
                 if not mod:
-                    new_states.append((ip+df.loc[ip, 'arg'], acc, visits + [ip], True))
+                    new_states.append((ip+args[ip], acc, visits + [ip], True))
         states = new_states
 # %%
 %%time
-run2(df) # 40ms
+run2(ops,args) # 5ms
 
 # %%
