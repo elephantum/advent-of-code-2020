8ef3f0454881cf0898de697781412d0f4e873cb9
Andrey Tatarinov <a@tatarinov.co>
2020-12-08 23:13:38 +0300

    08 no pandas

--- 08/08_bruteforce.py
+++ 08/08_bruteforce.py
@@ -1,44 +1,48 @@
 #%%
 import pandas as pd
 # %%
-df = pd.read_csv('input.txt', sep=' ', header=None, names=['op', 'arg'])
+df = pd.read_csv('./08/input.txt', sep=' ', header=None, names=['op', 'arg'])
+ops = list(df['op'])
+args = list(df['arg'])
 # %%
-def run(df):
-    df['visit'] = 0
+def run(ops, args):
+    visits = list([0] * len(ops))
     ip = 0
     acc = 0
 
     while True:
-        if ip == len(df):
+        if ip == len(ops):
             return True, acc
-        if ip > len(df) or df.loc[ip, 'visit'] == 1:
+        if ip > len(ops) or visits[ip] == 1:
             return False, acc
 
-        df.loc[ip, 'visit'] += 1
+        visits[ip] += 1
 
-        if df.loc[ip, 'op'] == 'acc':
-            acc += df.loc[ip, 'arg']
+        if ops[ip] == 'acc':
+            acc += args[ip]
             ip += 1
-        elif df.loc[ip, 'op'] == 'jmp':
-            ip += df.loc[ip, 'arg']
-        elif df.loc[ip, 'op'] == 'nop':
+        elif ops[ip] == 'jmp':
+            ip += args[ip]
+        elif ops[ip] == 'nop':
             ip += 1
-# %%
+#%%
+run(ops, args)
+#%%
 %%time
-for ip in range(len(df)):
-    if df.loc[ip, 'op'] == 'jmp':
-        df_test = df.copy()
-        df_test.loc[ip, 'op'] = 'nop'
-    elif df.loc[ip, 'op'] == 'nop':
-        df_test = df.copy()
-        df_test.loc[ip, 'op'] = 'jmp'
+for ip in range(len(ops)):
+    if ops[ip] == 'jmp':
+        ops_test = ops.copy()
+        ops_test[ip] = 'nop'
+    elif ops[ip] == 'nop':
+        ops_test = ops.copy()
+        ops_test[ip] = 'jmp'
     else:
         continue
 
-    (ok, acc) = run(df_test)
+    (ok, acc) = run(ops_test, args)
     if ok:
         break
 # %%
-# 7.67sec
+# 11.2ms
 ip, ok, acc
 # %%
