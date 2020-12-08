19b8f35f0f154c872001fc30afe19d4e6e3be421
Andrey Tatarinov <a@tatarinov.co>
2020-12-08 22:22:28 +0300

    08 bruteforce

---
+++ 08/08_bruteforce.py
@@ -0,0 +1,46 @@
+#%%
+import pandas as pd
+# %%
+df = pd.read_csv('input.txt', sep=' ', header=None, names=['op', 'arg'])
+# %%
+
+# %%
+def run(df):
+    df['visit'] = 0
+    ip = 0
+    acc = 0
+
+    while True:
+        if ip == len(df):
+            return True, acc
+        if ip > len(df) or df.loc[ip, 'visit'] == 1:
+            return False, acc
+
+        df.loc[ip, 'visit'] += 1
+
+        if df.loc[ip, 'op'] == 'acc':
+            acc += df.loc[ip, 'arg']
+            ip += 1
+        elif df.loc[ip, 'op'] == 'jmp':
+            ip += df.loc[ip, 'arg']
+        elif df.loc[ip, 'op'] == 'nop':
+            ip += 1
+# %%
+for ip in range(len(df)):
+    op =  df.loc[ip, 'op']
+ 
+    if op in ['jmp', 'nop']:
+        if op == 'jmp':
+            df_test = df.copy()
+            df_test.loc[ip, 'op'] = 'nop'
+        elif op == 'nop':
+            df_test = df.copy()
+            df_test.loc[ip, 'op'] = 'jmp'
+
+        (ok, acc) = run(df_test)
+        if ok:
+            break
+# %%
+# 7.67sec
+ip, ok, acc
+# %%
